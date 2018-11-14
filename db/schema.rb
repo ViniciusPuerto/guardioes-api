0f258fffee9267a3d740cc8964b8b288b513a8b8
Foxtrot40 <cabeceiramarcos@gmail.com>
2018-11-14 17:29:56 -0200

    generated App scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_11_14_192918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "apps", force: :cascade do |t|
+    t.string "app_name"
+    t.string "owner_country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
